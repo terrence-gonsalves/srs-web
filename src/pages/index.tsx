e7e7391f76db84e338c5bd0ba0724c52ba799dca
terrence.gonsalves <terrence.gonsalves@gmail.com>
2025-12-18 19:40:13 -0500

    updated <a> to <Link>

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,10 @@
+import Link from "next/link";
+
 export default function Home() {
   return (
     <main className="p-6">
       <h1 className="text-2xl font-bold">Salesforce Report Summarizer</h1>
-      <a href="/upload" className="text-blue-600 underline">Upload report</a>
+      <Link href="/upload" className="text-blue-600 underline">Upload report</Link>
     </main>
   );
 }
